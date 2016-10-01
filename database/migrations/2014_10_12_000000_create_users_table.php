524ec0ab7e1ae8fc95c49d5d07bb551fe98bf3a9
Al- Imran Ahmed <al.imran.cse@gmail.com>
2016-10-01 08:13:22 +0600

    Users table schema created in migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,24 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('title', 10);
+            $table->string('name')->index();
             $table->string('email')->unique();
             $table->string('password');
+            $table->integer('image_id')->unsigned()->nullable();
+            $table->foreign('image_id')
+                ->references('id')
+                ->on('images')
+                ->onDelete('cascade');
+            $table->integer('is_active')->unsigned()->default(1);
+            $table->dateTime('last_active')->nullable();
+            $table->string('last_ip', 200)->nullable();
+            $table->integer('location_id')->unsigned()->nullable();
+            $table->foreign('location_id')
+                ->references('id')
+                ->on('locations')
+                ->onDelete('cascade');
+            $table->string('token')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
