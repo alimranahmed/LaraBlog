4fb570b6a82f66148dc17e2badd89d8a59c3a73d
Al- Imran Ahmed <al.imran.cse@gmail.com>
2016-10-01 07:58:55 +0600

    All primary migration added

---
+++ database/migrations/2016_10_01_015709_create_image_posts_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateImagePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('image_post', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('image_post');
+    }
+}
