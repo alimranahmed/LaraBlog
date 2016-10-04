af051330efd00c00c2bc814918399adaa00b4150
Al- Imran Ahmed <al.imran.cse@gmail.com>
2016-10-05 00:07:16 +0600

    Routes Added for articles

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,5 @@ use Illuminate\Http\Request;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:api');
+
+Route::get('/article', 'ArticleController@index');
