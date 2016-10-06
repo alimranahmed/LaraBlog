849ae94eb32e25ab0de643894b8ce9dcb30199e9
Al- Imran Ahmed <al.imran.cse@gmail.com>
2016-10-06 21:18:51 +0600

    Association between article and comment has been made

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -7,7 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Article extends Model
 {
     protected $guarded = ['id'];
+
     protected function user(){
         return $this->belongsTo(User::class);
+    }
+
+    public function articles(){
+        return $this->hasMany(Comment::class);
     }
 }
--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    //
+    public function article(){
+        return $this->belongsTo(Article::class);
+    }
 }
