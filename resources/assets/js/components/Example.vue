4056ab74fc288672b018eb50851060aed3dbcb18
Al- Imran Ahmed <al.imran.cse@gmail.com>
2016-10-18 01:18:41 +0600

    Google icon added and button tested

--- resources/assets/js/components/Example.vue
+++ resources/assets/js/components/Example.vue
@@ -13,5 +13,10 @@
         <div class="col s1">11</div>
         <div class="col s1">12</div>
     </div>
+    <div>
+        <a class="waves-effect waves-light btn">button</a>
+        <a class="waves-effect waves-light btn"><i class="material-icons left">cloud</i>button</a>
+        <a class="waves-effect waves-light btn"><i class="material-icons right">cloud</i>button</a>
+    </div>
 </template>
 
