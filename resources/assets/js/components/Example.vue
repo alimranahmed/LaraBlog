03d67c832ac980a7a6830aba0ddf6591173d4cb2
Al- Imran Ahmed <al.imran.cse@gmail.com>
2016-10-19 00:51:34 +0600

    Navbar added

--- resources/assets/js/components/Example.vue
+++ resources/assets/js/components/Example.vue
@@ -1,22 +1,40 @@
 <template>
-    <div class="row">
-        <div class="col s1">1</div>
-        <div class="col s1">2</div>
-        <div class="col s1">3</div>
-        <div class="col s1">4</div>
-        <div class="col s1">5</div>
-        <div class="col s1">6</div>
-        <div class="col s1">7</div>
-        <div class="col s1">8</div>
-        <div class="col s1">9</div>
-        <div class="col s1">10</div>
-        <div class="col s1">11</div>
-        <div class="col s1">12</div>
+    <div class="navbar-fixed">
+        <nav>
+            <div class="nav-wrapper green lighten-5 purple-text text-darken-4">
+                <a href="#" class="brand-logo purple-text text-darken-4">Al- Imran Ahmed</a>
+                <ul id="nav-mobile" class="right hide-on-med-and-down">
+                    <li class="active">
+                        <a href="#" class="purple-text text-darken-4">Sass</a>
+                    </li>
+                    <li class="purple-text">
+                        <a href="#" class="purple-text text-darken-4">Components</a>
+                    </li>
+                    <li>
+                        <a class="dropdown-button purple-text text-darken-4" data-activates="dropdown1">Dropdown
+                            <i class="material-icons right">arrow_drop_down</i>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <ul id="dropdown1" class="dropdown-content">
+                <li><a href="#!">Profile</a></li>
+                <li><a href="#!">Settings</a></li>
+                <li class="divider"></li>
+                <li><a href="#!">Logout</a></li>
+            </ul>
+        </nav>
     </div>
-    <div>
-        <a class="waves-effect waves-light btn">button</a>
-        <a class="waves-effect waves-light btn"><i class="material-icons left">cloud</i>button</a>
-        <a class="waves-effect waves-light btn"><i class="material-icons right">cloud</i>button</a>
+    <div style="min-height:480px">
+
     </div>
+    <footer class="page-footer" style="padding:0px;">
+        <div class="footer-copyright green lighten-5">
+            <div class="container">
+                <span class="purple-text text-darken-4">© 2016 Al- Imran Ahmed</span>
+                <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+            </div>
+        </div>
+    </footer>
 </template>
 
