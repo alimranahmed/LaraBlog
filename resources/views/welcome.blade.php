9cd790b88749f15c1c2f61ac2123c733f465c7c1
Al- Imran Ahmed <al.imran.cse@gmail.com>
2016-10-22 23:53:05 +0600

    View now extending the layout page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,61 +1,6 @@
-<!DOCTYPE html>
-<html lang="en" xmlns="http://www.w3.org/1999/html">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>L5 Blog</title>
-        <!-- Fonts -->
-        <link href="{{asset("css/app.css")}}" rel="stylesheet" type="text/css">
-    </head>
-    <body>
-        <div id="app">
-            <nav class="navbar navbar-default navbar-fixed-top">
-                <div class="container-fluid">
-                    <!-- Brand and toggle get grouped for better mobile display -->
-                    <div class="navbar-header">
-                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                            <span class="sr-only">Toggle navigation</span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                        </button>
-                        <a class="navbar-brand" href="#">Al- Imran Ahmed</a>
-                    </div>
-
-                    <!-- Collect the nav links, forms, and other content for toggling -->
-                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                        <ul class="nav navbar-nav navbar-right">
-                            <li class="active"><a href="#">Link </a></li>
-                            <li><a href="#">Link</a></li>
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-                                <ul class="dropdown-menu">
-                                    <li><a href="#">Profile</a></li>
-                                    <li><a href="#">Settings</a></li>
-                                    <li role="separator" class="divider"></li>
-                                    <li><a href="#">Logout</a></li>
-                                </ul>
-                            </li>
-                        </ul>
-                        <form class="navbar-form navbar-right">
-                            <div class="form-group">
-                                <input type="text" class="form-control" placeholder="Search">
-                            </div>
-                        </form>
-                    </div><!-- /.navbar-collapse -->
-                </div><!-- /.container-fluid -->
-            </nav>
-            <div class="main-container container-fluid">
-                @foreach($articles as $article)
-                    <h1>{{$article->heading}}</h1>
-                @endforeach
-            </div>
-            <footer class="container-fluid">
-                <span class="purple-text text-darken-4">© 2016 Al- Imran Ahmed</span>
-            </footer>
-        </div>
-        <script src="{{asset("js/app.js")}}"></script>
-    </body>
-</html>
+@extends('layouts.main')
+@section('content')
+    @foreach($articles as $article)
+        <h1>{{$article->heading}}</h1>
+    @endforeach
+@endsection
