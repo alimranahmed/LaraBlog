95b1d6aafe15ba81e31e96c9e9cd6b2ee25c38d3
Al- Imran Ahmed <al.imran.cse@gmail.com>
2016-10-23 23:19:55 +0600

    Layout maintained in seperate files

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="container-fluid">
+    <span class="purple-text text-darken-4">© 2016 Al- Imran Ahmed</span>
+</footer>
+</div>
+<script src="{{asset("js/app.js")}}"></script>
+</body>
+</html>
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/html">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>L5 Blog</title>
+    <!-- Fonts -->
+    <link href="{{asset("css/app.css")}}" rel="stylesheet" type="text/css">
+</head>
+<body>
+<div id="l5_blog">
