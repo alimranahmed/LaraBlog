d39b49c41e6869bda000bc872cba25d50b9a3354
Al- Imran Ahmed <al.imran.cse@gmail.com>
2016-10-25 23:34:11 +0600

    Publication datetime is being shown in the article list

--- resources/views/frontend/articles.blade.php
+++ resources/views/frontend/articles.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.public')
 @section('content')
     @foreach($articles as $article)
-        <a href="{{route('get-article', $article->id)}}"><h1>{{$article->heading}}</h1></a>
+        <a href="{{route('get-article', $article->id)}}">
+            <h2>{{$article->heading}}</h2>
+            <span>published on {{$article->published_at}}</span>
+        </a>
     @endforeach
 @endsection
