4b33869a7222a6414d2b2519ccf679af4f6f10a5
Al- Imran Ahmed <al.imran.cse@gmail.com>
2016-10-27 22:49:42 +0600

    comment is being added

--- resources/views/frontend/article.blade.php
+++ resources/views/frontend/article.blade.php
@@ -8,4 +8,18 @@
     <div class="row">
         <div class="col-sm-12">{{$article->content}}</div>
     </div>
+    <div class="row">
+        <div class="col-sm-12">
+            <h4>Comments</h4>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-12">
+            @forelse($article->comments as $comment)
+                {{$comment->content}}
+            @empty
+                <span class="text-grey">No comment yet<span>
+            @endforelse
+        </div>
+    </div>
 @endsection
