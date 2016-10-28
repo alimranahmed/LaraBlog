72267e109cb59f08a1cd32b8aeeabedeefb1b31b
Al- Imran Ahmed <al.imran.cse@gmail.com>
2016-10-28 23:14:51 +0600

    Comment table seeder created associated with post

---
+++ database/seeds/CommentsTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class CommentsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(\App\Models\Comment::class, 10)->create();
+    }
+}
