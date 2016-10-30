f7177868fcf1519ca1707eb9b06290f48aa64ca3
Al- Imran Ahmed <al.imran.cse@gmail.com>
2016-10-30 08:49:30 +0600

    Comment is being displaying and form placement fixed

--- resources/views/frontend/article.blade.php
+++ resources/views/frontend/article.blade.php
@@ -13,10 +13,34 @@
             <h4>Comments</h4>
         </div>
     </div>
+    <div class="row">
+        <div class="col-sm-12">
+            <form action="{{route('add-comment', $article->id)}}" method="post" name="add_comment_form">
+                {{csrf_field()}}
+                <div class="form-group {{$errors->has('content') ? 'has-error has-feedback' : ''}}">
+                    <textarea name="content" class="form-control" id="comment"
+                              placeholder="Comment*">{{old('content')}}</textarea>
+                </div>
+                <div class="form-group">
+                    <input type="text" name="name" value="{{old('name')}}" class="form-control" id="name"
+                           placeholder="Name">
+                </div>
+                <div class="form-group {{$errors->has('email') ? 'has-error has-feedback' : ''}}">
+                    <input type="email" name="email" value="{{old('email')}}" class="form-control" id="email"
+                           placeholder="Email">
+                </div>
+                <button type="submit" class="btn btn-primary">Comment</button>
+            </form>
+        </div>
+    </div>
     @forelse($article->comments as $comment)
         <div class="row">
-            <div class="col-sm-12">{{$comment->content}}</div>
-            <div class="col-sm-12">published at {{$comment->created_at}}</div>
+            <div class="col-sm-2">
+                <b>{{is_null($comment->user) ? 'Someone' : $comment->user->name }}</b>&nbsp;said:
+            </div>
+            <div class="col-sm-12">{{$comment->content}}
+                <span class="text-grey">&nbsp;at&nbsp;{{$comment->created_at}}</span>
+            </div>
         </div>
     @empty
         <div class="row">
@@ -25,18 +49,4 @@
             </div>
         </div>
     @endforelse
-    <div class="row">
-        <div class="col-sm-12">
-            <form action="{{route('add-comment', $article->id)}}" method="post">
-                {{csrf_field()}}
-                <div class="form-group">
-                    <textarea type="text" name="content" class="form-control" id="comment" placeholder="Comment"></textarea>
-                </div>
-                <div class="form-group">
-                    <input type="text" name="name" class="form-control" id="name" placeholder="Name">
-                </div>
-                <button type="submit" class="btn btn-primary">Commit</button>
-            </form>
-        </div>
-    </div>
 @endsection
--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -1,7 +1,7 @@
 @include('layouts._header')
 @include('layouts._navbar')
 {{--@include('layouts._sidebar')--}}
-<div class="main-container container-fluid">
+<div class="main-container container-fluid" id="app">
     @yield('content')
 </div>
 @include('layouts._footer')
