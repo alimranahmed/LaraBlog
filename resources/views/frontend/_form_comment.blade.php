8d73c0da47315988ffde3886cf91d6e1be2a8b03
Al- Imran Ahmed <al.imran.cse@gmail.com>
2016-11-01 21:47:41 +0600

    Notify me button added

--- resources/views/frontend/_form_comment.blade.php
+++ resources/views/frontend/_form_comment.blade.php
@@ -1,18 +1,22 @@
 <div class="col-sm-12">
     <form action="{{route('add-comment', $article->id)}}" method="post" name="add_comment_form">
         {{csrf_field()}}
-        <div class="form-group {{$errors->has('content') ? 'has-error has-feedback' : ''}}">
-                    <textarea name="content" class="form-control" id="comment"
-                              placeholder="Comment*">{{old('content')}}</textarea>
+        <div class="form-group col-sm-12 no-padding {{$errors->has('content') ? 'has-error has-feedback' : ''}}">
+            <textarea name="content" class="form-control" id="comment" rows="3"
+                      placeholder="Comment*">{{old('content')}}</textarea>
         </div>
-        <div class="form-group">
-            <input type="text" name="name" value="{{old('name')}}" class="form-control" id="name"
-                   placeholder="Name">
+        <div class="form-group col-sm-6 no-padding-left">
+            <input type="text" name="name" value="{{old('name')}}" class="form-control" id="name" placeholder="Name">
         </div>
-        <div class="form-group {{$errors->has('email') ? 'has-error has-feedback' : ''}}">
+        <div class="form-group col-sm-6 no-padding-right {{$errors->has('email') ? 'has-error has-feedback' : ''}}">
             <input type="email" name="email" value="{{old('email')}}" class="form-control" id="email"
                    placeholder="Email">
         </div>
-        <button type="submit" class="btn btn-primary pull-right">Comment</button>
+        <div class="form-group col-sm-6 col-sm-offset-6 no-padding-right checkbox">
+            <label>
+                <input type="checkbox"> Notify me about new article
+            </label>
+            <button type="submit" class="btn btn-primary pull-right">Comment</button>
+        </div>
     </form>
 </div>
