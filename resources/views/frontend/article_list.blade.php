df3784ba7b057db3ced2b4d4d98c895cf0c09cd2
Al- Imran Ahmed <al.imran.cse@gmail.com>
2016-11-08 00:19:42 +0600

    Article list has been moved to seperate place to reuse

---
+++ resources/views/frontend/article_list.blade.php
@@ -0,0 +1,15 @@
+@forelse($articles as $article)
+    <a href="{{route('get-article', $article->id)}}">
+        <div class="row article-list">
+            <h3 class="heading">{{$article->heading}}</h3>
+            <span class="time">published {{$article->published_at}}</span>
+            <span class="text-grey {{!$article->hit_count ? 'hide':''}}">
+                (visited {{$article->hit_count}} time)
+            </span>
+        </div>
+    </a>
+@empty
+    <div class="row text-grey">
+        <h3>Nothing Found</h3>
+    </div>
+@endforelse
--- resources/views/frontend/articles.blade.php
+++ resources/views/frontend/articles.blade.php
@@ -1,14 +1,4 @@
 @extends('layouts.public')
 @section('content')
-    @foreach($articles as $article)
-        <a href="{{route('get-article', $article->id)}}">
-            <div class="row article-list">
-                <h3 class="heading">{{$article->heading}}</h3>
-                <span class="time">published {{$article->published_at}}</span>
-                <span class="text-grey {{!$article->hit_count ? 'hide':''}}">
-                    (visited {{$article->hit_count}} time)
-                </span>
-            </div>
-        </a>
-    @endforeach
+    @include('frontend.article_list', ['articles' => $articles])
 @endsection
