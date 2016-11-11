14539a0dcb3edda1889b80c456ee71bdeb60364a
Al- Imran Ahmed <al.imran.cse@gmail.com>
2016-11-11 22:38:46 +0600

    getting all categories in app service has been removed

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        View::share('categories', Category::all());
+        //View::share('categories', Category::all());
     }
 
     /**
