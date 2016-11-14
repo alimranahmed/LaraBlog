57514012b95fd6850f01fb8e971cb607cc85c010
Al- Imran Ahmed <al.imran.cse@gmail.com>
2016-11-14 21:54:02 +0600

    Login form created

--- resources/views/backend/auth.blade.php
+++ resources/views/backend/auth.blade.php
@@ -6,7 +6,22 @@
                 <strong>Welcome</strong>
             </div>
             <div class="panel-body">
-
+                <form>
+                    <div class="input-group">
+                        <div class="input-group-addon">@</div>
+                        <input type="email" name="email" class="form-control" id="email" placeholder="Email">
+                    </div>
+                    <div class="input-group margin-top-15">
+                        <div class="input-group-addon"><i class="fa fa-lock"></i></div>
+                        <input type="password" name="password" class="form-control" id="password" placeholder="Password">
+                    </div>
+                    <div class="checkbox">
+                        <label>
+                            <input type="checkbox" name="remember_me"> Remember Me
+                        </label>
+                    </div>
+                    <button type="submit" class="btn btn-success pull-right">Login</button>
+                </form>
             </div>
         </div>
     </div>
