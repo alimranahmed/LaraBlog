45c4fe34f19136ffbbe4390324cb610a8cebc578
Al- Imran Ahmed <al.imran.cse@gmail.com>
2016-11-16 10:27:40 +0600

    Email and password error in login page is being heighlited

--- resources/views/backend/auth.blade.php
+++ resources/views/backend/auth.blade.php
@@ -9,11 +9,11 @@
                 <form method="post" action="{{route('login')}}">
                     {{csrf_field()}}
                     <div class="text-danger">{{session('auth_error')}}</div>
-                    <div class="input-group">
+                    <div class="input-group {{$errors->has('email') ? 'has-error has-feedback' : ''}}">
                         <div class="input-group-addon">@</div>
-                        <input type="email" name="email" class="form-control" id="email" placeholder="Email">
+                        <input type="email" name="email" class="form-control" id="email" value="{{old('email')}}" placeholder="Email">
                     </div>
-                    <div class="input-group margin-top-15">
+                    <div class="input-group margin-top-15 {{$errors->has('password') ? 'has-error has-feedback' : ''}}">
                         <div class="input-group-addon"><i class="fa fa-lock"></i></div>
                         <input type="password" name="password" class="form-control" id="password" placeholder="Password">
                     </div>
