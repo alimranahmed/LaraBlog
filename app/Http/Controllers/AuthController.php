58fdeb0ac4ce8214994550cacb8f4a65c0b533ab
Al- Imran Ahmed <al.imran.cse@gmail.com>
2016-11-16 10:39:21 +0600

    Remember me feature applied

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -15,7 +15,7 @@ class AuthController extends Controller
     public function login(Request $request){
         $this->validate($request, ['email' => 'required|email', 'password' => 'required|min:4']);
 
-        if(Auth::attempt($request->only('email', 'password'))){
+        if(Auth::attempt($request->only('email', 'password'), $request->has('remember_me'))){
             return Auth::user();
         }else{
             return back()->with('auth_error','Invalid credentials');
