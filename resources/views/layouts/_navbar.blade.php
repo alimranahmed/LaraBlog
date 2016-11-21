991e6cfeda3ccb259ae51d999509f6e298b2130a
Al- Imran Ahmed <al.imran.cse@gmail.com>
2016-11-21 10:35:37 +0600

    Logged in user's name is being displayed in the navbar

--- resources/views/layouts/_navbar.blade.php
+++ resources/views/layouts/_navbar.blade.php
@@ -28,7 +28,7 @@
                 @if(Auth::check())
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
-                           aria-expanded="false">Username <span class="caret"></span></a>
+                           aria-expanded="false">{{Auth::user()->name}} <span class="caret"></span></a>
                         <ul class="dropdown-menu">
                             <li><a href="#">Profile</a></li>
                             <li><a href="#">Settings</a></li>
