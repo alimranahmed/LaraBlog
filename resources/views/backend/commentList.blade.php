b8a17c547d2b3643a66d0ad47a7f51afe25aa277
Al- Imran Ahmed <al.imran.cse@gmail.com>
2016-11-25 01:17:18 +0600

    Comment list view added

---
+++ resources/views/backend/commentList.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.admin')
+@section('content')
+    <table class="table table-hover table-bordered">
+        <tr class="text-center">
+            <th>ID</th>
+            <th>Comment</th>
+            <th>By</th>
+            <th>Commented</th>
+            <th>Published</th>
+            <th>Article</th>
+            <th>Operations</th>
+        </tr>
+        @foreach($comments as $comment)
+            <tr>
+                <td>{{$comment->id}}</td>
+                <td>{{$comment->content}}</td>
+                <td>{{$comment->user->email}}</td>
+                <td class="text-center">{{$comment->createdAtHuman}}</td>
+                <td class="text-center">
+                    <span class="fa {{$comment->is_published ? 'fa-check text-success' : 'fa-times text-danger'}}"></span>
+                </td>
+                <td>{{substr($comment->article->heading, 0, 20)}}...</td>
+                <td class="text-center">
+                    <span class="fa fa-edit text-primary"></span>&nbsp;
+                    <span class="fa fa-trash text-danger"></span>
+                </td>
+            </tr>
+        @endforeach
+    </table>
+@endsection
