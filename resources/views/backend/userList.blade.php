51eba18fa48d6a9803de5731d3f5fbe2fb926fea
Al- Imran Ahmed <al.imran.cse@gmail.com>
2016-11-27 10:21:24 +0600

    User list view created

---
+++ resources/views/backend/userList.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.admin')
+@section('content')
+    <table class="table table-hover table-bordered">
+        <tr class="text-center">
+            <th>ID</th>
+            <th>Name</th>
+            <th>Username</th>
+            <th>Email</th>
+            <th>Created</th>
+            <th class="text-center">Operations</th>
+        </tr>
+        @foreach($users as $user)
+            <tr>
+                <td>{{$user->id}}</td>
+                <td>{{$user->name}}</td>
+                <td>{{$user->email}}</td>
+                <td>{{$user->createdAtHuman}}</td>
+                <td>
+                    <a href="{{route('articles-by-category', $user->alias)}}">{{$user->articles->count()}}</a>
+                </td>
+                <td class="text-center">
+                    <span class="fa fa-edit text-primary"></span>&nbsp;
+                    <span class="fa fa-trash text-danger"></span>
+                </td>
+            </tr>
+        @endforeach
+    </table>
+@endsection
