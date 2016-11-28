6ca86769099e88db0a0255b96358f3bf627595bf
Al- Imran Ahmed <al.imran.cse@gmail.com>
2016-11-28 09:35:23 +0600

    CreatedAtHuman has bee added in keyword model

--- app/Models/Keyword.php
+++ app/Models/Keyword.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class Keyword extends Model
@@ -10,5 +11,10 @@ class Keyword extends Model
 
     public function articles(){
         return $this->belongsToMany(Article::class, 'article_keyword');
+    }
+
+    public function getCreatedAtHumanAttribute(){
+        $createdAt = new Carbon($this->created_at);
+        return $createdAt->diffForHumans(Carbon::now());
     }
 }
