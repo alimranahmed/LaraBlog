188c997fac376bc2148da71bb7117a142f56a10b
Al- Imran Ahmed <al.imran.cse@gmail.com>
2016-12-04 00:28:02 +0600

    Front end form validation added

--- resources/views/frontend/_form_comment.blade.php
+++ resources/views/frontend/_form_comment.blade.php
@@ -3,14 +3,14 @@
         {{csrf_field()}}
         <div class="form-group col-sm-12 no-padding {{$errors->has('content') ? 'has-error has-feedback' : ''}}">
             <textarea name="content" class="form-control" id="comment" rows="3"
-                      placeholder="Comment*">{{old('content')}}</textarea>
+                      placeholder="Comment*" required>{{old('content')}}</textarea>
         </div>
         <div class="form-group col-sm-6 no-padding-left {{$errors->has('name') ? 'has-error has-feedback' : ''}}">
-            <input type="text" name="name" value="{{old('name')}}" class="form-control" id="name" placeholder="Name*">
+            <input type="text" name="name" value="{{old('name')}}" class="form-control" id="name" placeholder="Name*" required>
         </div>
         <div class="form-group col-sm-6 no-padding-right {{$errors->has('email') ? 'has-error has-feedback' : ''}}">
             <input type="email" name="email" value="{{old('email')}}" class="form-control" id="email"
-                   placeholder="Email*">
+                   placeholder="Email*" required>
         </div>
         <div class="form-group col-sm-6 col-sm-offset-6 no-padding-right checkbox">
             <label>
