59cc7d71e819d6812a970bd2d0dc52b6a97f3b24
Al- Imran Ahmed <al.imran.cse@gmail.com>
2016-12-05 23:08:02 +0600

    Alert message layout created

--- resources/views/layouts/_alert.blade.php
+++ resources/views/layouts/_alert.blade.php
@@ -1,5 +1,16 @@
-<div class="alert alert-success" id="success-alert">
-    <button type="button" class="close" data-dismiss="alert">x</button>
-    <strong>Success! </strong>
-    Product have added to your wishlist.
-</div>
+@if(session('successMsg'))
+    <div class="alert alert-success" id="success-alert">
+        <button type="button" class="close" data-dismiss="alert">x</button>
+        <strong>Success! </strong>{{session('successMsg')}}
+    </div>
+@elseif(session('errorMsg'))
+    <div class="alert alert-danger" id="success-alert">
+        <button type="button" class="close" data-dismiss="alert">x</button>
+        <strong>Error; </strong>{{session('errorMsg')}}
+    </div>
+@elseif(session('warningMsg'))
+    <div class="alert alert-warning" id="success-alert">
+        <button type="button" class="close" data-dismiss="alert">x</button>
+        <strong>Warning, </strong>{{session('warningMsg')}}
+    </div>
+@elseif
