36514bf5ee6d1eb1632332ac7b4831ee825caf5b
Al- Imran Ahmed <al.imran.cse@gmail.com>
2016-12-05 23:07:23 +0600

    Alert message layout created

---
+++ resources/views/layouts/_alert.blade.php
@@ -0,0 +1,5 @@
+<div class="alert alert-success" id="success-alert">
+    <button type="button" class="close" data-dismiss="alert">x</button>
+    <strong>Success! </strong>
+    Product have added to your wishlist.
+</div>
