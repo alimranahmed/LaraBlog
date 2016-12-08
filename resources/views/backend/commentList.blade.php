bc47832b32a5bdbfc6a0c920012548af637d3175
Al- Imran Ahmed <al.imran.cse@gmail.com>
2016-12-08 07:59:52 +0600

    Comment edit implemented in view

--- resources/views/backend/commentList.blade.php
+++ resources/views/backend/commentList.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.admin')
 @section('content')
-    <table class="table table-hover table-bordered">
+    <table class="table table-hover table-bordered" id="commentList">
         <tr class="text-center">
             <th>ID</th>
             <th>Comment</th>
@@ -25,7 +25,7 @@
                     <a href="{{route('get-article', $comment->article->id)}}">{{substr($comment->article->heading, 0, 20)}}...</a>
                 </td>
                 <td class="text-center">
-                    <span class="fa fa-edit text-primary"></span>&nbsp;
+                    <span class="fa fa-edit text-primary pointer" v-on:click="showCommentForm({{$comment}})"></span>&nbsp;
                     <a href="{{route('delete-comment', $comment->id)}}" onclick="return confirm('Are you sure to delete?')">
                         <span class="fa fa-trash text-danger"></span>
                     </a>
@@ -33,4 +33,46 @@
             </tr>
         @endforeach
     </table>
+@endsection
+<!-- Modal -->
+<div class="modal fade" id="comment-modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
+                            aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title" id="myModalLabel">Edit Comment</h4>
+            </div>
+            <form id="comment-form" method="POST">
+                <div class="modal-body">
+                    {{csrf_field()}}
+                    <input type="hidden" name="_method" value="PUT">
+                    <div class="form-group">
+                        <label>Content</label>
+                        <textarea name="content" placeholder="Content" id="content" class="form-control"></textarea>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-primary">Save changes</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+@section('inPageJS')
+    <script>
+        new Vue({
+            el: "#commentList",
+            data: {},
+            methods: {
+                showCommentForm: function(comment){
+                    $("#content").val(comment.content);
+                    $("#comment-form").attr("action", "comment/" + comment.id);
+                    $("#comment-modal").modal("show");
+                }
+            }
+        });
+    </script>
 @endsection
