133768df1619290a661216a2e935db43a3f7774c
Al- Imran Ahmed <al.imran.cse@gmail.com>
2016-12-11 21:18:02 +0600

    User delete functionality added

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,11 +4,24 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
     public function index(Request $request){
         $users = User::with('role')->get();
         return view('backend.userList', compact('users'));
+    }
+
+    public function destroy(Request $request, $userId){
+        if(Auth::user()->id == $userId){
+            return redirect()->back()->with('errorMsg', 'You cannot delete yourself');
+        }
+        try{
+            User::destroy($userId);
+        }catch(\PDOException $e){
+            return redirect()->back()->with('errorMsg', $this->getMessage($e));
+        }
+        return redirect()->back()->with('successMsg', 'User deleted');
     }
 }
