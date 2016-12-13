78f7910cf26da56c2a8cd24203b348aca54bcca4
Al- Imran Ahmed <al.imran.cse@gmail.com>
2016-12-13 21:13:22 +0600

    Article heading text size changed

--- resources/views/frontend/article.blade.php
+++ resources/views/frontend/article.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     <div class="row">
         <div class="col-sm-12">
-            <h3 class="article-heading text-lg">{{$article->heading}}</h3>
+            <h1 class="article-heading text-xlg">{{$article->heading}}</h1>
         </div>
     </div>
     <div class="row">
