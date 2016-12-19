8cc9aba68753d3e4b6a54ff1d3a4f4eae6822fab
Al- Imran Ahmed <al.imran.cse@gmail.com>
2016-12-19 23:06:39 +0600

    Vuejs csrf header added

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,10 +1,12 @@
 require('./libraries');
 
 
-window.Laravel = {csrfToken: '{{ csrf_token() }}'};
+/*window.Laravel = {csrfToken: '{{ csrf_token() }}'};
 
 Vue.http.interceptors.push(function (request, next) {
     request.headers['X-CSRF-TOKEN'] = Laravel.csrfToken;
 
     next();
-});
+});*/
+
+Vue.http.headers.common['X-CSRF-TOKEN'] = document.querySelector('#token').getAttribute('content');
--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta id="token" name="token" content="{{ csrf_token() }}">
 
     <title>Al- Imran Ahmed</title>
     <!-- Fonts -->
