7aa94b426573b3a55ae1d3c72baed594133d2df2
Al- Imran Ahmed <al.imran.cse@gmail.com>
2016-12-23 15:21:07 +0600

    Article add button added in admin's article list

--- resources/views/backend/articleList.blade.php
+++ resources/views/backend/articleList.blade.php
@@ -1,42 +1,48 @@
 @extends('layouts.admin')
 @section('content')
-    <table class="table table-hover table-bordered">
-        <tr class="text-center">
-            <th>ID</th>
-            <th>Title</th>
-            <th>Category</th>
-            <th>Written</th>
-            <th>Is Published</th>
-            <th>Published</th>
-            <th>Edited</th>
-            <th>Comments</th>
-            <th>Hits</th>
-            <th>Operations</th>
-        </tr>
-        @foreach($articles as $article)
-            <tr>
-                <td>{{$article->id}}</td>
-                <td>{{$article->heading}}</td>
-                <td>{{$article->category->name}}</td>
-                <td class="text-center">{{$article->createdAtHuman}}</td>
-                <td class="text-center">
-                    <a href="{{route('toggle-article-publish', $article->id)}}">
-                        <strong class="fa fa-lg {{$article->is_published ? 'fa-toggle-on text-success' : 'fa-toggle-off text-grey'}}"></strong>
-                    </a>
-                </td>
-                <td class="text-center">
-                    <span class="{{!$article->is_published?'hide':''}}">{{$article->publishedAtHuman}}</span>
-                </td>
-                <td class="text-center">{{$article->updatedAtHuman}}</td>
-                <td class="text-center">{{$article->comment_count}}</td>
-                <td class="text-center">{{$article->hit_count}}</td>
-                <td class="text-center">
-                    <span class="fa fa-edit text-primary"></span>&nbsp;
-                    <a href="{{route('delete-article', $article->id)}}" onclick="return confirm('Are you sure to delete?')">
-                        <span class="fa fa-trash text-danger"></span>
-                    </a>
-                </td>
+    <div class="row">
+        <a href="{{route('create-article')}}" class="margin-top-5 margin-bottom-5 btn btn-success">Post New Article</a>
+    </div>
+    <div class="row">
+        <table class="table table-hover table-bordered">
+            <tr class="text-center">
+                <th>ID</th>
+                <th>Title</th>
+                <th>Category</th>
+                <th>Written</th>
+                <th>Is Published</th>
+                <th>Published</th>
+                <th>Edited</th>
+                <th>Comments</th>
+                <th>Hits</th>
+                <th>Operations</th>
             </tr>
-        @endforeach
-    </table>
+            @foreach($articles as $article)
+                <tr>
+                    <td>{{$article->id}}</td>
+                    <td>{{$article->heading}}</td>
+                    <td>{{$article->category->name}}</td>
+                    <td class="text-center">{{$article->createdAtHuman}}</td>
+                    <td class="text-center">
+                        <a href="{{route('toggle-article-publish', $article->id)}}">
+                            <strong class="fa fa-lg {{$article->is_published ? 'fa-toggle-on text-success' : 'fa-toggle-off text-grey'}}"></strong>
+                        </a>
+                    </td>
+                    <td class="text-center">
+                        <span class="{{!$article->is_published?'hide':''}}">{{$article->publishedAtHuman}}</span>
+                    </td>
+                    <td class="text-center">{{$article->updatedAtHuman}}</td>
+                    <td class="text-center">{{$article->comment_count}}</td>
+                    <td class="text-center">{{$article->hit_count}}</td>
+                    <td class="text-center">
+                        <span class="fa fa-edit text-primary"></span>&nbsp;
+                        <a href="{{route('delete-article', $article->id)}}"
+                           onclick="return confirm('Are you sure to delete?')">
+                            <span class="fa fa-trash text-danger"></span>
+                        </a>
+                    </td>
+                </tr>
+            @endforeach
+        </table>
+    </div>
 @endsection
