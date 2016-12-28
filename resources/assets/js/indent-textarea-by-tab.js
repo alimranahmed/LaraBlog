4df1bdf5b94ea0fe536e644b48c18be57da699ac
Al- Imran Ahmed <al.imran.cse@gmail.com>
2016-12-28 23:04:49 +0600

    Cursor is being 4 space ahead when tab pressed

--- resources/assets/js/indent-textarea-by-tab.js
+++ resources/assets/js/indent-textarea-by-tab.js
@@ -8,11 +8,10 @@ $(document).delegate('.textarea-indent', 'keydown', function(e) {
 
         // set textarea value to: text before caret + tab + text after caret
         $(this).val($(this).val().substring(0, start)
-            + "\t"
+            + "    "
             + $(this).val().substring(end));
 
-        // put caret at right position again
-        $(this).get(0).selectionStart =
-            $(this).get(0).selectionEnd = start + 1;
+        // put caret at right position again(4 space)
+        $(this).get(0).selectionStart = $(this).get(0).selectionEnd = start + 4;
     }
 });
