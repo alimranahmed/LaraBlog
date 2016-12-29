df33b659e37679487275c85de91642a546349b52
Al- Imran Ahmed <al.imran.cse@gmail.com>
2016-12-30 01:04:36 +0600

    New permissions added for categories, keywords, comments and users

--- database/seeds/PermissionsTableSeeder.php
+++ database/seeds/PermissionsTableSeeder.php
@@ -12,20 +12,25 @@ class PermissionsTableSeeder extends Seeder
     public function run()
     {
         $permissions = [
+            [ 'name' => 'article-read', 'display_name' => '', 'description' => '',],
             [ 'name' => 'article-create', 'display_name' => '', 'description' => '',],
             [ 'name' => 'article-edit', 'display_name' => '', 'description' => '',],
             [ 'name' => 'article-delete', 'display_name' => '', 'description' => '',],
-            [ 'name' => 'article-publish', 'display_name' => '', 'description' => '',],
+            [ 'name' => 'category-read', 'display_name' => '', 'description' => '',],
             [ 'name' => 'category-create', 'display_name' => '', 'description' => '',],
             [ 'name' => 'category-edit', 'display_name' => '', 'description' => '',],
             [ 'name' => 'category-delete', 'display_name' => '', 'description' => '',],
-            [ 'name' => 'category-publish', 'display_name' => '', 'description' => '',],
-            [ 'name' => 'article-create', 'display_name' => '', 'description' => '',],
-            [ 'name' => 'article-create', 'display_name' => '', 'description' => '',],
-            [ 'name' => 'article-create', 'display_name' => '', 'description' => '',],
-            [ 'name' => 'article-create', 'display_name' => '', 'description' => '',],
-            [ 'name' => 'article-create', 'display_name' => '', 'description' => '',],
-            [ 'name' => 'article-create', 'display_name' => '', 'description' => '',],
+            [ 'name' => 'comment-read', 'display_name' => '', 'description' => '',],
+            [ 'name' => 'comment-create', 'display_name' => '', 'description' => '',],
+            [ 'name' => 'comment-edit', 'display_name' => '', 'description' => '',],
+            [ 'name' => 'comment-delete', 'display_name' => '', 'description' => '',],
+            [ 'name' => 'keyword-read', 'display_name' => '', 'description' => '',],
+            [ 'name' => 'keyword-create', 'display_name' => '', 'description' => '',],
+            [ 'name' => 'keyword-edit', 'display_name' => '', 'description' => '',],
+            [ 'name' => 'keyword-read', 'display_name' => '', 'description' => '',],
+            [ 'name' => 'keyword-create', 'display_name' => '', 'description' => '',],
+            [ 'name' => 'keyword-edit', 'display_name' => '', 'description' => '',],
+            [ 'name' => 'keyword-delete', 'display_name' => '', 'description' => '',],
         ];
     }
 }
