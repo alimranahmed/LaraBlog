b744f8f83f794c273b26e266de9554f7790c2924
Al- Imran Ahmed <al.imran.cse@gmail.com>
2016-12-29 11:27:31 +0600

    Permission table initiated

--- database/seeds/PermissionsTableSeeder.php
+++ database/seeds/PermissionsTableSeeder.php
@@ -11,6 +11,21 @@ class PermissionsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $permissions = [
+            [ 'name' => 'article-create', 'display_name' => '', 'description' => '',],
+            [ 'name' => 'article-edit', 'display_name' => '', 'description' => '',],
+            [ 'name' => 'article-delete', 'display_name' => '', 'description' => '',],
+            [ 'name' => 'article-publish', 'display_name' => '', 'description' => '',],
+            [ 'name' => 'category-create', 'display_name' => '', 'description' => '',],
+            [ 'name' => 'category-edit', 'display_name' => '', 'description' => '',],
+            [ 'name' => 'category-delete', 'display_name' => '', 'description' => '',],
+            [ 'name' => 'category-publish', 'display_name' => '', 'description' => '',],
+            [ 'name' => 'article-create', 'display_name' => '', 'description' => '',],
+            [ 'name' => 'article-create', 'display_name' => '', 'description' => '',],
+            [ 'name' => 'article-create', 'display_name' => '', 'description' => '',],
+            [ 'name' => 'article-create', 'display_name' => '', 'description' => '',],
+            [ 'name' => 'article-create', 'display_name' => '', 'description' => '',],
+            [ 'name' => 'article-create', 'display_name' => '', 'description' => '',],
+        ];
     }
 }
