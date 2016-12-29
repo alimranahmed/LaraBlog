09422ebf77529cf7a1a3769f691ea4ce1f1383ae
Al- Imran Ahmed <al.imran.cse@gmail.com>
2016-12-30 02:46:55 +0600

    Syntext highlighting added for code has been added

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -4,6 +4,8 @@
 </div>
 <script src="{{asset("js/vue.js")}}"></script>
 <script src="{{asset("js/app.js")}}" type="text/javascript"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.9.0/highlight.min.js"></script>
+<script>hljs.initHighlightingOnLoad();</script>
 @yield('inPageJS')
 </body>
 </html>
