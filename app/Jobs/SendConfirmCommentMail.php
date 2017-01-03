a95eef0799b8c99484d75ffca1b5149b4689c7bb
Al- Imran Ahmed <al.imran.cse@gmail.com>
2017-01-04 00:49:16 +0600

    Send comment confirmation email job created

---
+++ app/Jobs/SendConfirmCommentMail.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Mail\CommentConfirmation;
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Support\Facades\Mail;
+
+class SendConfirmCommentMail implements ShouldQueue
+{
+    use InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public $comment;
+
+    public function __construct($comment)
+    {
+        $this->comment = $comment;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        Mail::to($this->comment->user->email)->send(new CommentConfirmation($this->comment));
+    }
+}
