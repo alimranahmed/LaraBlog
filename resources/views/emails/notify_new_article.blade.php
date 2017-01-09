6f0040e778d21bb4470e1490cf643b9817fa1328
Al- Imran Ahmed <al.imran.cse@gmail.com>
2017-01-09 23:53:42 +0600

    New article email layout changed

--- resources/views/emails/notify_new_article.blade.php
+++ resources/views/emails/notify_new_article.blade.php
@@ -3,7 +3,9 @@
     <head>
     </head>
     <body>
-        <h2>A new Article has been published in Imran's Blog</h2>
-        <p>Check this out</p>
+        <h2>A new Article has been published in Imran's Blog about {{$article->category->name}}</h2>
+        <h2>{{$article->heading}}<h2>
+        <p>{{substr($article->content, 0, 50)}}...</p>
+        <a href="{{route('get-article', [$article->id])}}">Read More</a>
     <body>
 </html>
