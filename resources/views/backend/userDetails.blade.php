959454ce5e1f695af2f33b9fb22a734bd4b21636
Al- Imran Ahmed <al.imran.cse@gmail.com>
2017-01-13 23:47:07 +0600

    User's last ip is being shown

--- resources/views/backend/userDetails.blade.php
+++ resources/views/backend/userDetails.blade.php
@@ -1,15 +1,23 @@
 @extends("layouts.admin")
 @section("content")
-    <div class="row">
-        <div class="col-sm-2">Name:</div>
+    <div class="row margin-top-10">
+        <div class="col-sm-1 text-right"><strong>Name</strong></div>
         <div class="col-sm-4">{{$user->name}}</div>
     </div>
     <div class="row">
-        <div class="col-sm-2">Email:</div>
+        <div class="col-sm-1 text-right"><strong>Username</strong></div>
+        <div class="col-sm-4">{{$user->username}}</div>
+    </div>
+    <div class="row">
+        <div class="col-sm-1 text-right"><strong>Last IP</strong></div>
+        <div class="col-sm-4">{{$user->last_ip}}</div>
+    </div>
+    <div class="row">
+        <div class="col-sm-1 text-right"><strong>Email</strong></div>
         <div class="col-sm-4">{{$user->email}}</div>
     </div>
     <div class="row">
-        <div class="col-sm-2">Roles:</div>
+        <div class="col-sm-1 text-right"><strong>Roles</strong></div>
         <div class="col-sm-4">{{implode(',',$user->roles->pluck('display_name')->toArray())}}</div>
     </div>
 @endsection
