bf954415ec506459c7c7358d5875efaf60e71015
Al- Imran Ahmed <al.imran.cse@gmail.com>
2017-01-18 23:58:31 +0600

    User edit button is being shon in user details

--- resources/views/backend/userDetails.blade.php
+++ resources/views/backend/userDetails.blade.php
@@ -1,8 +1,12 @@
 @extends("layouts.admin")
 @section("content")
     <div class="row margin-top-10">
-        <div class="col-sm-1 text-right"><strong>Name</strong></div>
-        <div class="col-sm-4">{{$user->name}}</div>
+        <div class="col-sm-4">
+            <strong class="text-lg">{{$user->name}}</strong>
+            <a href="{{route('edit-user', ['userId' => $user->id])}}">
+                <span class="fa fa-edit text-primary"></span>
+            </a>
+        </div>
     </div>
     <div class="row">
         <div class="col-sm-1 text-right"><strong>Username</strong></div>
