a0a37e0805f900c6dbb7de6923e3033e4c85125f
Al- Imran Ahmed <al.imran.cse@gmail.com>
2017-01-19 21:44:17 +0600

    Sidebar color has not match with full height

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -3,7 +3,7 @@
 @include('layouts._alert')
 <div class="main-container container-fluid no-padding">
     <h1 class="text-center border-bottom" id="site-heading">Al- Imran Ahmed</h1>
-    <aside class="col-sm-2 no-padding">
+    <aside class="col-sm-2 no-padding sidebar-container">
         @if(Auth::check())
             @include('layouts._sidebar')
         @endif
