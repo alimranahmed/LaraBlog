c3aa1c731776bed8d4f7bd22a5c165cee7a3d02a
Al- Imran Ahmed <al.imran.cse@gmail.com>
2017-01-20 13:00:18 +0600

    User validation added

---
+++ app/Http/Requests/UserRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        $rules = [
+            'role_id' => 'required',
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required',
+        ];
+        return $rules;  
+    }
+}
