2ad38578700d5c612cf45fa4f9c6e5b8f87a5027
Al- Imran Ahmed <al.imran.cse@gmail.com>
2017-01-23 00:47:11 +0600

    Article Test class created

--- tests/ArticleTest.php
+++ tests/ArticleTest.php
@@ -4,16 +4,15 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
-class ExampleTest extends TestCase
+class ArticleTest extends TestCase
 {
     /**
-     * A basic functional test example.
+     * A basic test example.
      *
      * @return void
      */
-    public function testBasicExample()
+    public function testExample()
     {
-        $this->visit('/')
-             ->see('Al- Imran Ahmed (-');
+        //$this->assertTrue(true);
     }
 }
