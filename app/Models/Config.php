10d108cf5cc5acdf285eb3948aec2c9854e237f2
Al- Imran Ahmed <al.imran.cse@gmail.com>
2017-02-26 22:14:22 +0600

    #16: Config get function created

--- app/Models/Config.php
+++ app/Models/Config.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Config extends Model
 {
-    //
+    public static function get($name){
+        $config = self::where('name', $name)->first();
+        if(is_null($config)){
+            return null;
+        }
+        return $config->value;
+    }
 }
