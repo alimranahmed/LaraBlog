97c7cf42d50d395a44f3f815f5e34b6bee95db6c
Al- Imran Ahmed <al.imran.cse@gmail.com>
2017-02-27 22:26:01 +0600

    #13: feedback migration and model created

---
+++ database/migrations/2017_02_27_161927_create_feedback_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFeedbackTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('feedbacks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->nullable();
+            $table->string('email');
+            $table->text('content');
+            $table->text('ip')->nullable();
+            $table->integer('is_resolved')->unsigned()->default(0);
+            $table->integer('is_closed')->unsigned()->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('feedbacks');
+    }
+}
