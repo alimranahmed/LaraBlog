f9df3b613813d8ada84194a1d08417770403cebd
Al- Imran Ahmed <al.imran.cse@gmail.com>
2017-03-01 21:13:58 +0600

    Comment confirmation email view cleaned up

--- resources/views/emails/comment_confirmation.blade.php
+++ resources/views/emails/comment_confirmation.blade.php
@@ -1,11 +1,5 @@
 <!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-</head>
 <body>
     <h2>Hi, <strong>{{$comment->user->name}}</strong></h2>
     <p>A comment has been published on Al- Imran's Blog from you mail. Click the link bellow to confirm your comment</p>
