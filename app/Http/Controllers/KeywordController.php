576b98c65afd1140fe13daeff15375eb21a993e9
Al- Imran Ahmed <al.imran.cse@gmail.com>
2017-03-03 09:58:03 +0600

    #11: Article can be shown by keyword

--- app/Http/Controllers/KeywordController.php
+++ app/Http/Controllers/KeywordController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\KeywordRequest;
+use App\Models\Article;
 use App\Models\Keyword;
 
 class KeywordController extends Controller
@@ -49,5 +50,20 @@ class KeywordController extends Controller
             return redirect()->back()->with('errorMsg', $this->getMessage($e));
         }
         return redirect()->route('keywords')->with('successMsg', 'Keyword deleted');
+    }
+
+    public function getArticles($keywordName){
+        $keyword = Keyword::where('name', $keywordName)->first();
+        if(is_null($keyword)){
+            return redirect()->route('home')->with('warningMsg', 'Keyword not found');
+        }
+        $articleIds = $keyword->articles->pluck('id')->toArray();
+        $articles = Article::whereIn('id', $articleIds)
+            ->where('is_deleted', 0)
+            ->where('is_published', 1)
+            ->orderBy('created_at', 'desc')
+            ->paginate(15);
+
+        return view('frontend.articles', compact('articles'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,7 @@ Route::get('article', 'ArticleController@index')->name('articles');
 Route::get('article/{articleId}', 'ArticleController@show')->name('get-article');
 Route::get('confirm-comment/{commentId}', 'CommentController@confirmComment')->name('confirm-comment');
 Route::get('category/article/{categoryAlias}', 'CategoryController@getArticles')->name('articles-by-category');
+Route::get('keyword/article/{keywordName}', 'KeywordController@getArticles')->name('articles-by-keyword');
 
 Route::get('search', 'ArticleController@search')->name('search-article');
 //Comment
