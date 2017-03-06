d59dff57e6e1c3e7a560308665578f7b2369e8d4
Al- Imran Ahmed <al.imran.cse@gmail.com>
2017-03-06 22:14:15 +0600

    #17: Global variable change being reflect in everywhere

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -2,7 +2,7 @@
 @include('layouts._navbar')
 @include('layouts._alert')
 <div class="main-container container-fluid no-padding">
-    <h1 class="text-center border-bottom" id="site-heading">{{$configs->site_title}}</h1>
+    <h1 class="text-center border-bottom" id="site-heading">{{$globalConfigs->site_title}}</h1>
     <aside class="col-sm-2 no-padding sidebar-container">
         @if(Auth::check())
             @include('layouts._sidebar')
