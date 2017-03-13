d7dcee1f921739f84a3deeaddadeafe615dd7f04
Al- Imran Ahmed <al.imran.cse@gmail.com>
2017-03-13 22:02:58 +0600

    #18: Article list is now more informative

--- resources/views/frontend/_article_list.blade.php
+++ resources/views/frontend/_article_list.blade.php
@@ -1,13 +1,29 @@
 @forelse($articles as $article)
-    <a href="{{route('get-article', $article->id)}}">
-        <div class="row article-list">
-            <h3 class="heading">{{$article->heading}}</h3>
-            <span class="time">published {{$article->publishedAtHuman}}</span>
-            <span class="text-grey {{!$article->hit_count ? 'hide':''}}">
-                ({{$article->hit_count}} {{$article->hit_count > 1 ? 'reads' : 'read'}})
-            </span>
+    <div class="row article-list">
+        <div class="col-sm-12">
+            <div class="row">
+                <a href="{{route('get-article', $article->id)}}">
+                    <h3 class="heading">{{$article->heading}}</h3>
+                </a>
+            </div>
+            <div class="row margin-top-5">
+                <div class="col-sm-4 no-padding">
+                    @foreach($article->keywords as $keyword)
+                        <a href="{{route('articles-by-keyword', [$keyword->name])}}">
+                            <span class="label label-info">{{$keyword->name}}</span>
+                        </a>
+                    @endforeach
+                </div>
+                <div class="text-right col-sm-8 no-padding">
+                    <span class="time">published {{$article->publishedAtHuman}} on</span>
+                    <a href="{{route('articles-by-category', $article->category->alias)}}">{{$article->category->name}}</a>
+                    <span class="text-grey {{!$article->hit_count ? 'hide':''}}">
+                        ({{$article->hit_count}} {{$article->hit_count > 1 ? 'reads' : 'read'}})
+                    </span>
+                </div>
+            </div>
         </div>
-    </a>
+    </div>
 @empty
     <div class="row text-grey">
         <div class="col-sm-12"><h3>Not Available</h3></div>
