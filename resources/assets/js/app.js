360d35c3d7cd857cf7cfdecdc00ffb3d72f41dc3
Al- Imran Ahmed <al.imran.cse@gmail.com>
2017-03-17 00:17:13 +0600

    broadcast is being listen

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -10,3 +10,18 @@ Vue.http.interceptors.push(function (request, next) {
 });*/
 
 Vue.http.headers.common['X-CSRF-TOKEN'] = document.querySelector('#token').getAttribute('content');
+
+import Echo from "laravel-echo"
+
+window.Pusher = require('pusher-js');
+
+window.Echo = new Echo({
+    broadcaster: 'pusher',
+    key: 'bbadb783c3924ad225f7',
+    cluster: 'mt1'
+});
+
+window.Echo.private('visitor-activity')
+    .listen('comment-on-article', function(e){
+        console.debug(e);
+});
