d3a98be14ec8737e826ee266117417b1335734fe
Al- Imran Ahmed <al.imran.cse@gmail.com>
2017-03-16 10:10:57 +0600

    Pusher config changed

--- config/app.php
+++ config/app.php
@@ -174,7 +174,7 @@ return [
          */
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
+        App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
