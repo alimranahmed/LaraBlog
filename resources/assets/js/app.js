b3170a9e1e25b15b6ba579351e85f5b16bfa030e
Al- Imran Ahmed <al.imran.cse@gmail.com>
2017-03-19 09:09:55 +0600

    Pusher js is retriving pusher messages

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,32 +1,18 @@
 require('./libraries');
 
-
-/*window.Laravel = {csrfToken: '{{ csrf_token() }}'};
-
-Vue.http.interceptors.push(function (request, next) {
-    request.headers['X-CSRF-TOKEN'] = Laravel.csrfToken;
-
-    next();
-});*/
-
 Vue.http.headers.common['X-CSRF-TOKEN'] = document.querySelector('#token').getAttribute('content');
 
-import Echo from "laravel-echo"
+
+
 
 window.Pusher = require('pusher-js');
 
-var pusher = new Pusher('bbadb783c3924ad225f7', {
-    authEndpoint: "/broadcasting/auth",
-    encrypted: true
-});
-/*
-window.Echo = new Echo({
-    broadcaster: 'pusher',
-    key: 'bbadb783c3924ad225f7',
-    cluster: 'mt1'
-});
+var pusher = new Pusher('bbadb783c3924ad225f7', {encrypted: true});
 
-window.Echo.private('visitor-activity')
-    .listen('comment-on-article', function(e){
-        console.debug(e);
-});*/
+// Pusher.logToConsole = true;
+
+var channel = pusher.subscribe('visitor-activity');
+channel.bind('comment', function(data) {
+    console.debug(data);
+    $("#new-comment").show();
+});
