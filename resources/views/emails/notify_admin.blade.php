0dcc852f435be85e78bfddd41754f2f89a68d501
Al- Imran Ahmed <al.imran.cse@gmail.com>
2017-03-22 23:18:50 +0600

    #19: Notify email admin view added

---
+++ resources/views/emails/notify_admin.blade.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html lang="en">
+<body>
+<h2>Hi, <strong>{{$globalConfigs->copyright_owner}}</strong></h2>
+<p>A user has respond on {{$globalConfigs->site_name}}</p>
+<h3>Response:</h3>
+<p>{{$message}}</p>
+<a href="{{$url}}">Click to see</a>
+</body>
+</html>
