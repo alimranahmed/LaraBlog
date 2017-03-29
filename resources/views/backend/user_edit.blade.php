d8830e14149925cfe6637f9787108f44be902ba2
Al- Imran Ahmed <al.imran.cse@gmail.com>
2017-03-29 22:47:42 +0600

    #21: User edit desing issue fixed

--- resources/views/backend/user_edit.blade.php
+++ resources/views/backend/user_edit.blade.php
@@ -5,33 +5,33 @@
             <strong>Edit Information of {{$user->name}}</strong>
         </div>
         <div class="panel-body">
+            <form action="{{route('update-user', ['userId' => $user->id])}}" class="margin-top-15" method="post">
+                <input type="hidden" name="_method" value="PUT">
+                {{csrf_field()}}
+                <div class="form-group {{$errors->has('role_id') ? "has-error" : ""}}">
+                    <select name="role_id" class="form-control">
+                        <option value="" readonly="true">Select User Role*</option>
+                        @foreach($roles as $role)
+                            <option value="{{$role->id}}" {{$user->roles->first()->id == $role->id ? 'selected' : ''}}>{{$role->display_name}}</option>
+                        @endforeach
+                    </select>
+                </div>
+                <div class="form-group {{$errors->has('name') ? "has-error" : ""}}">
+                    <input type="text" name="name" id="name" value="{{$user->name}}" class="form-control" placeholder="Name*">
+                </div>
+                <div class="form-group {{$errors->has('username') ? "has-error" : ""}}">
+                    <input type="text" name="username" id="username" value="{{$user->username}}" class="form-control" placeholder="Username">
+                </div>
+                <div class="form-group {{$errors->has('email') ? "has-error" : ""}}">
+                    <input type="text" name="email" id="email" value="{{$user->email}}" class="form-control" placeholder="Email*">
+                </div>
+                <div class="form-group {{$errors->has('password') ? "has-error" : ""}}">
+                    <input type="password" name="password" id="password" value="" class="form-control" placeholder="Password">
+                </div>
+                <div class="form-group {{$errors->has('role_id') ? "has-error" : ""}}">
+                    <button class="btn btn-success" type="submit">Update</button>
+                </div>
+            </form>
         </div>
     </div>
-    <form action="{{route('update-user', ['userId' => $user->id])}}" class="margin-top-15" method="post">
-        <input type="hidden" name="_method" value="PUT">
-        {{csrf_field()}}
-        <div class="form-group {{$errors->has('role_id') ? "has-error" : ""}}">
-            <select name="role_id" class="form-control">
-                <option value="" readonly="true">Select User Role*</option>
-                @foreach($roles as $role)
-                    <option value="{{$role->id}}" {{$user->roles->first()->id == $role->id ? 'selected' : ''}}>{{$role->display_name}}</option>
-                @endforeach
-            </select>
-        </div>
-        <div class="form-group {{$errors->has('name') ? "has-error" : ""}}">
-            <input type="text" name="name" id="name" value="{{$user->name}}" class="form-control" placeholder="Name*">
-        </div>
-        <div class="form-group {{$errors->has('username') ? "has-error" : ""}}">
-            <input type="text" name="username" id="username" value="{{$user->username}}" class="form-control" placeholder="Username">
-        </div>
-        <div class="form-group {{$errors->has('email') ? "has-error" : ""}}">
-            <input type="text" name="email" id="email" value="{{$user->email}}" class="form-control" placeholder="Email*">
-        </div>
-        <div class="form-group {{$errors->has('password') ? "has-error" : ""}}">
-            <input type="password" name="password" id="password" value="" class="form-control" placeholder="Password">
-        </div>
-        <div class="form-group {{$errors->has('role_id') ? "has-error" : ""}}">
-            <button class="btn btn-success" type="submit">Update</button>
-        </div>
-    </form>
 @endsection
