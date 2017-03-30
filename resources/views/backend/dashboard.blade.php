6b79b968655a0892e8f819b22e109fc25ae954f8
Al- Imran Ahmed <al.imran.cse@gmail.com>
2017-03-30 09:49:48 +0600

    Welcome message added in the dashbaord

--- resources/views/backend/dashboard.blade.php
+++ resources/views/backend/dashboard.blade.php
@@ -2,9 +2,10 @@
 @section('content')
     <div class="panel panel-default no-margin-bottom">
         <div class="panel-heading">
-            <strong>Reports</strong>
+            <strong>Welcome!</strong>
         </div>
         <div class="panel-body">
+            <h2>Welcome to laraBlog's Admin portal!</h2>
         </div>
     </div>
 @endsection
