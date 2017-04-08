4779106ff82885818eb61b4350bbb59dc9de98fb
Al- Imran Ahmed <al.imran.cse@gmail.com>
2017-04-08 09:09:52 +0600

    Login page changed, empty sidebar is hidden

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -3,12 +3,12 @@
 @include('layouts._alert')
 <div class="main-container container-fluid no-padding">
     <h1 class="text-center border-bottom" id="site-heading">{{$globalConfigs->site_title}}</h1>
-    <aside class="col-sm-2 no-padding sidebar-container">
-        @if(Auth::check())
+    @if(Auth::check())
+        <aside class="col-sm-2 no-padding sidebar-container">
             @include('layouts._sidebar')
-        @endif
-    </aside>
-    <div class="col-sm-10 no-padding">
+        </aside>
+    @endif
+    <div class="{{Auth::check() ? '' : 'col-sm-offset-2'}} col-sm-10 no-padding">
         @yield('content')
     </div>
 </div>
