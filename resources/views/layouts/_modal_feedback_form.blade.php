401e9aef31b146ad764400dc93c33ed5788ba4aa
Al- Imran Ahmed <al.imran.cse@gmail.com>
2017-04-24 13:57:52 +0600

    Feedback form validated in html end

--- resources/views/layouts/_modal_feedback_form.blade.php
+++ resources/views/layouts/_modal_feedback_form.blade.php
@@ -8,9 +8,9 @@
             <form method="post" action="{{route('add-feedback')}}">
                 {{csrf_field()}}
                 <div class="modal-body">
-                    <input type="text" placeholder="Name*" name="name" class="form-control">
-                    <input type="email" placeholder="Email*" name="email" class="form-control margin-top-10">
-                    <textarea placeholder="Feedback*" class="form-control margin-top-10" name="content"></textarea>
+                    <input type="text" placeholder="Name*" name="name" class="form-control" required>
+                    <input type="email" placeholder="Email*" name="email" class="form-control margin-top-10" required>
+                    <textarea placeholder="Feedback*" class="form-control margin-top-10" name="content" required></textarea>
                 </div>
                 <div class="modal-footer">
                     <button type="submit" class="btn btn-primary">Subscribe</button>
