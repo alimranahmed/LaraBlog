3c166b42c7083ea9d974056af12fb85d8357e047
Al- Imran Ahmed <al.imran.cse@gmail.com>
2017-04-27 22:52:25 +0600

    #10: Required data is being loaded for reports

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -1,11 +1,19 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Article;
+use App\Models\Category;
+use App\Models\Comment;
+use App\Models\HitLogger;
 use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
     public function index(){
+        $categories = Category::all();
+        $comments = Comment::all();
+        $articles = Article::all();
+        $hitLoggers = HitLogger::all();
         return view('backend.dashboard');
     }
 }
