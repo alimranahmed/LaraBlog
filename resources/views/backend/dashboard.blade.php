1c6a9e57e86ced664b9e762ae00bb0c75c698fa7
Al- Imran Ahmed <al.imran.cse@gmail.com>
2017-04-29 00:01:28 +0600

    #10: Country wize hit report is being shown

--- resources/views/backend/dashboard.blade.php
+++ resources/views/backend/dashboard.blade.php
@@ -2,10 +2,27 @@
 @section('content')
     <div class="panel panel-default no-margin-bottom">
         <div class="panel-heading">
-            <strong>Welcome!</strong>
+            <strong>Welcome to laraBlog's Admin portal!</strong>
         </div>
         <div class="panel-body">
-            <h2>Welcome to laraBlog's Admin portal!</h2>
+            <div class="col-sm-4">
+                <div class="panel panel-default no-margin-bottom">
+                    <div class="panel-heading">
+                        <strong>Country vs Hits</strong>
+                    </div>
+                    <div class="panel-body">
+                        <table class="table">
+                            <tr>
+                                <th class="text-left">Country Name</th>
+                                <th class="text-left">Hit Count</th>
+                            </tr>
+                            @foreach($hitCountries as $key => $country)
+                                <tr><td>{{$key}}</td><td>{{count($country)}}</td></tr>
+                            @endforeach
+                        </table>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
