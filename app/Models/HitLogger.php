4e65b1ea369dd92c9e37bfa4242d0d8c39f50ac4
Al- Imran Ahmed <al.imran.cse@gmail.com>
2017-04-28 23:40:29 +0600

    Hitlogger is returning with country name

--- app/Models/HitLogger.php
+++ app/Models/HitLogger.php
@@ -7,11 +7,20 @@ use Illuminate\Database\Eloquent\Model;
 class HitLogger extends Model
 {
     protected $guarded = ['id'];
+    protected $appends = ['country', 'city'];
     public function article(){
         return $this->belongsTo(Article::class);
     }
 
     public function address(){
         return $this->belongsTo(Address::class);
+    }
+
+    public function getCountryAttribute(){
+        return isset($this->address->country_name) ? $this->address->country_name : 'Unknown';
+    }
+
+    public function getCityAttribute(){
+        return isset($this->address->city) ? $this->address->city : 'Unknown';
     }
 }
