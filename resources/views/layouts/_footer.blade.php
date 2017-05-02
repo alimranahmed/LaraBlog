d2db7d8a8241cfc345e145e1706c8424ce05f7b2
Al- Imran Ahmed <al.imran.cse@gmail.com>
2017-05-02 23:41:11 +0600

    Footer's all text are centered now

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,17 +1,17 @@
 <footer class="container-fluid border-top">
-    <div class="row purple-text text-darken-4">
-        <div class="col-sm-2">Powered by
+    <div class="row purple-text text-darken-4 footer">
+        <div class="col-sm-2 text-center">Powered by
             <a href="https://github.com/alimranahmed/LaraBlog" target="_blank">LaraBlog</a>
         </div>
-        <div class="col-sm-2">© {{date('Y').' '.$globalConfigs->copyright_owner}} </div>
+        <div class="col-sm-2 text-center">© {{date('Y').' '.$globalConfigs->copyright_owner}} </div>
         @if(!Auth::check())
-            <div class="col-sm-1">
+            <div class="col-sm-1 text-center">
                 <span class="pointer text-info" data-toggle="modal" data-target="#subscribe-form">Subscribe</span>
             </div>
-            <div class="col-sm-1">
+            <div class="col-sm-1 text-center">
                 <span class="pointer text-info" data-toggle="modal" data-target="#feedback-form">Feedback</span>
             </div>
-            <div class="col-sm-1 col-sm-offset-5">
+            <div class="col-sm-1 col-sm-offset-5 text-center">
                 <a href="{{route('login-form')}}" class="text-grey">Manage</a>
             </div>
         @endif
