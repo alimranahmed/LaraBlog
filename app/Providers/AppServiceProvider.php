c35290f45a2f3dd37adcec400db8cc805765ebd2
Al- Imran Ahmed <al.imran.cse@gmail.com>
2017-05-11 12:12:14 +0600

    Bug sung published and added in appServiceProvider register method

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -32,6 +32,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->alias('bugsnag.multi', \Illuminate\Contracts\Logging\Log::class);
+        $this->app->alias('bugsnag.multi', \Psr\Log\LoggerInterface::class);
     }
 }
