899317e462f808b9fb22fc2dc340f527862e087f
Al- Imran Ahmed <al.imran.cse@gmail.com>
2017-05-12 22:13:14 +0600

    #25: Html error fixed in new article publication notification

--- resources/views/emails/notify_new_article.blade.php
+++ resources/views/emails/notify_new_article.blade.php
@@ -8,5 +8,5 @@
         <h2>{{$article->heading}}</h2>
         <div>{{substr($article->content, 0, 100)}}...</div>
         <a href="{{route('get-article', [$article->id])}}">Read More</a>
-    <body>
+    </body>
 </html>
