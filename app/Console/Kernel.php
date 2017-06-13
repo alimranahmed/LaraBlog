d06efcf06056eca2474615056e143d4b460b09e2
Al- Imran Ahmed <al.imran.cse@gmail.com>
2017-06-14 00:21:51 +0600

    #27: Backup command added in scheduler

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,8 +26,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-         $schedule->command('get-ip-location')
-                  ->hourly();
+         $schedule->command('get-ip-location')->hourly();
+         $schedule->command('backup:site-data')->weekly();
     }
 
     /**
