409c18893184a70ab738a7ada3605d6fbb5ebcd4
Al- Imran Ahmed <al.imran.cse@gmail.com>
2017-06-17 16:12:42 +0600

    #39: is_column enabled added in article table

---
+++ database/migrations/2017_06_17_100129_add_is_comment_enabled_in_article_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddIsCommentEnabledInArticleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('articles', function(Blueprint $table){
+            $table->integer('is_comment_enabled')
+                ->default(1)
+                ->unsigned();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('articles', function(Blueprint $table){
+            $table->dropColumn('is_comment_enabled');
+        });
+    }
+}
