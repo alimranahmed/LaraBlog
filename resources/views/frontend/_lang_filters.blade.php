58a8dd85a0e1cb0a41b7f2e28b6b99a1fcada62d
Al- Imran Ahmed <al.imran.cse@gmail.com>
2017-11-16 08:06:13 +0600

    #34: language filter hidden in small divice

--- resources/views/frontend/_lang_filters.blade.php
+++ resources/views/frontend/_lang_filters.blade.php
@@ -1,4 +1,4 @@
-<div class="lang-filter">
+<div class="lang-filter hidden-xs hidden-sm">
     <a class="btn btn-sm btn-default {{!request('lang') ? 'active' : ''}}" href="{{route('articles')}}">All</a>
 
     @foreach(config('fields.lang') as $lang => $fullLang)
