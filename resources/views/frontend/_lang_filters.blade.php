f4ff394096405ab7bd614951cd10955dc01ddce4
Al- Imran Ahmed <al.imran.cse@gmail.com>
2017-11-19 10:02:41 +0600

    #34: Article view edited to make fully functional the _lang_filter

--- resources/views/frontend/_lang_filters.blade.php
+++ resources/views/frontend/_lang_filters.blade.php
@@ -1,8 +1,11 @@
 <div class="lang-filter hidden-xs hidden-sm">
-    <a class="btn btn-sm btn-default {{!request('lang') ? 'active' : ''}}" href="{{route('articles')}}">All</a>
-
+    <a class="btn btn-sm btn-default {{!request('lang') ? 'active' : ''}}" href="{{url(request()->url())}}">All</a>
     @foreach(config('fields.lang') as $lang => $fullLang)
+        @php
+            $route = request()->route();
+            $fullUrl = route($route->action['as'], array_merge($route->parameters, ['lang' => $lang]));
+        @endphp
         <a class="btn btn-sm btn-default {{request('lang') == $lang ? 'active' : ''}}"
-           href="{{route('articles', ['lang' => $lang])}}">{{$fullLang}}</a>
+           href="{{$fullUrl}}">{{$fullLang}}</a>
     @endforeach
 </div>
