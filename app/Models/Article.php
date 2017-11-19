b1901c2931baaff19e7040db4ad977ee52fe0fb0
Al- Imran Ahmed <al.imran.cse@gmail.com>
2017-11-19 10:01:23 +0600

    #34: requried scope added and other filter used

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
 
@@ -40,6 +41,14 @@ class Article extends Model
         return $this->hasMany(HitLogger::class);
     }
 
+    public function scopeNotDeleted(Builder $builder){
+        return $builder->where('is_deleted', 0);
+    }
+
+    public function scopePublished(Builder $builder){
+        return $builder->where('is_published', 1);
+    }
+
     public function getPublishedAtHumanAttribute($value){
         $carbonDate = new Carbon($this->published_at);
         return $carbonDate->diffForHumans();
@@ -58,17 +67,42 @@ class Article extends Model
         return $this->category->name;
     }
 
-    public static function getPaginate(Request $request, $perPage = 15){
-        $articleQuery = Article::where('is_published', 1)->where('is_deleted', 0);
+    public static function getPaginate(Request $request){
+        $perPage = config('view.item_per_page');
+
+        $categoryAlias = $request->route('categoryAlias');
+        $keywordName = $request->route('keywordName');
+        if(!is_null($categoryAlias)){
+            $category = Category::where('alias', $categoryAlias)->first();
+            if(is_null($category)){
+                return collect([]);
+            }
+            $articleQuery = Article::where('category_id', $category->id);
+
+        }elseif(!is_null($keywordName)){
+            $keyword = Keyword::where('name', $keywordName)->first();
+            if(is_null($keyword)){
+                return collect([]);
+            }
+            $articleIds = $keyword->articles->pluck('id')->toArray();
+            $articleQuery = Article::whereIn('id', $articleIds);
+
+        } else{
+            $articleQuery = Article::published()->notDeleted();
+
+        }
+
+
         $paginateUrl = '';
         if($request->has('lang')){
             $articleQuery = $articleQuery->where('language', $request->lang);
             $paginateUrl = '?lang='.$request->lang;
         }
-        $articles = $articleQuery->orderBy('published_at', 'desc')
-            ->orderBy('created_at', 'desc')
+
+        $articles = $articleQuery->latest()
             ->paginate($perPage)
             ->withPath($paginateUrl);
+
         return $articles;
     }
 }
