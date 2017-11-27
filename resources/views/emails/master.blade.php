aada465829bdfb3d7922d0894b5ce5f220ba0d28
Al- Imran Ahmed <al.imran.cse@gmail.com>
2017-11-27 04:25:11 +0000

    Mail border color changed to match the blog color combination

--- resources/views/emails/master.blade.php
+++ resources/views/emails/master.blade.php
@@ -1,10 +1,10 @@
 <!doctype html>
 <html lang="en">
 <body>
-<div style="border: 1px solid #ddd; padding: 15px 40px">
+<div style="border: 1px solid #d1ebd3; padding: 15px 40px">
     @yield('content')
 </div>
-<div style="background: #ddd; padding: 15px; text-align: center">
+<div style="background: #d1ebd3; padding: 15px; text-align: center">
     © {{date('Y').' '.$globalConfigs->copyright_owner}}
 </div>
 </body>
