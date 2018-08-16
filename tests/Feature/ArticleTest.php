cc961136cd5f68e2ce346d3aea49d8a14a0c10aa
Al- Imran Ahmed <al.imran.cse@gmail.com>
2018-08-16 01:16:36 +0000

    Test written for article index

--- tests/Feature/ArticleTest.php
+++ tests/Feature/ArticleTest.php
@@ -2,19 +2,38 @@
 
 namespace Tests\Feature;
 
+use App\Models\Article;
+use Faker\Factory;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ArticleTest extends TestCase
 {
+    use DatabaseTransactions;
+
     /**
      * A basic test example.
      *
      * @return void
      */
-    public function testExample()
+    public function testIndex()
     {
-        $this->assertTrue(true);
+        factory(Article::class, 1)->state('published')->create([
+            'heading' => 'Test Heading',
+        ]);
+
+
+        factory(Article::class, 1)->state('unpublished')->create([
+            'heading' => 'Unpublished Heading',
+        ]);
+
+        $this->get('/article')
+            ->assertSee('Test Heading')
+            ->assertDontSee('Unpublished Heading');
     }
+
+
 }
