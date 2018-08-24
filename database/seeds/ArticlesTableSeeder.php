d6ee97410b3ca27b9f7ed1357a92b9c438af0bed
Al- Imran Ahmed <al.imran.cse@gmail.com>
2018-08-24 01:16:56 +0000

    Seeder issue fixed

--- database/seeds/ArticlesTableSeeder.php
+++ database/seeds/ArticlesTableSeeder.php
@@ -10,10 +10,13 @@ class ArticlesTableSeeder extends Seeder {
      * @return void
      */
     public function run() {
+        $faker = \Faker\Factory::create();
         if(env("APP_ENV") == 'local') {
-            factory(\App\Models\Article::class, 10)->create();
+            factory(\App\Models\Article::class, 10)->create([
+                'category_id' => $faker->randomElement(\App\Models\Category::all()->pluck('id')),
+                'user_id' => $faker->randomElement(\App\Models\User::all()->pluck('id')),
+            ]);
             $articles = Article::all();
-            $faker = \Faker\Factory::create();
             foreach ($articles as $article) {
                 $article->keywords()->attach($faker->numberBetween(1, 5));
             }
--- database/seeds/CommentsTableSeeder.php
+++ database/seeds/CommentsTableSeeder.php
@@ -11,9 +11,13 @@ class CommentsTableSeeder extends Seeder
      */
     public function run()
     {
+        $faker = Faker\Factory::create();
+
         DB::statement('SET FOREIGN_KEY_CHECKS=0;');
         if(env("APP_ENV") == 'local') {
-            factory(\App\Models\Comment::class, 30)->create();
+            factory(\App\Models\Comment::class, 30)->create([
+                'article_id' => $faker->randomElement(\App\Models\Article::all()->pluck('id'))
+            ]);
         }
         DB::statement('SET FOREIGN_KEY_CHECKS=1;');
     }
