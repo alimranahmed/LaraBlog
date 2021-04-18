09fca036b78473e68babb40934cb51e2d4939970
Al Imran Ahmed <al.imran.cse@gmail.com>
2021-04-18 12:29:48 +0600

    Config updated

--- config/blog.php
+++ config/blog.php
@@ -1,9 +1,17 @@
 <?php
 
 return [
+
     'item_per_page' => 15,
 
-    //This config is responsible for setting up the frontend design. Available design's are tailwindcss, bootstrap
-    'frontend_design' => env('FRONTEND_DESIGN', 'bootstrap'),
-//    'frontend_design' => env('FRONTEND_DESIGN', 'tailwindcss'),
+    /*
+    |-----------------------------------------------------------------
+    | Frontend Design Template
+    |-----------------------------------------------------------------
+    |
+    | This config is responsible for setting up the frontend design.
+    | Available design's are tailwindcss, bootstrap
+    */
+    //'frontend_design' => env('FRONTEND_DESIGN', 'bootstrap'),
+    'frontend_design' => env('FRONTEND_DESIGN', 'tailwindcss'),
 ];
