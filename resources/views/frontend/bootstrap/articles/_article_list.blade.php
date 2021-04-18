18f4fb4d753b1d9f20eb4679f9c0de7db1aed492
Al Imran Ahmed <al.imran.cse@gmail.com>
2021-04-18 12:34:24 +0600

    List card added in bootstrap

--- resources/views/frontend/bootstrap/articles/_article_list.blade.php
+++ resources/views/frontend/bootstrap/articles/_article_list.blade.php
@@ -1,27 +1,5 @@
 @forelse($articles as $article)
-    <div class="row article-list">
-        <div class="col-sm-12">
-            <div class="row">
-                <a href="{{route('get-article', [$article->id, make_slug($article->heading)])}}">
-                    <h3 class="heading">{{$article->heading}}</h3>
-                </a>
-            </div>
-            <div class="row margin-top-5">
-                <div class="col-md-8 no-padding">
-                    <span class="time">published {{$article->publishedAtHuman}} on</span>
-                    <a href="{{route('articles-by-category', $article->category->alias)}}">{{$article->category->name}}</a>
-                    <span class="text-grey"> by {{$article->user->name}}</span>
-                </div>
-                <div class="text-right col-md-4 no-padding">
-                    @foreach($article->keywords as $keyword)
-                        <a href="{{route('articles-by-keyword', [$keyword->name])}}">
-                            <span class="label label-info">{{$keyword->name}}</span>
-                        </a>
-                    @endforeach
-                </div>
-            </div>
-        </div>
-    </div>
+    @include("frontend.bootstrap.articles._list_card")
 @empty
     <div class="row text-grey">
         <div class="col-sm-12"><h3>Not Available</h3></div>
---
+++ resources/views/frontend/bootstrap/articles/_list_card.blade.php
@@ -0,0 +1,23 @@
+<div class="row article-list">
+    <div class="col-sm-12">
+        <div class="row">
+            <a href="{{route('get-article', [$article->id, make_slug($article->heading)])}}">
+                <h3 class="heading">{{$article->heading}}</h3>
+            </a>
+        </div>
+        <div class="row margin-top-5">
+            <div class="col-md-8 no-padding">
+                <span class="time">published {{$article->publishedAtHuman}} on</span>
+                <a href="{{route('articles-by-category', $article->category->alias)}}">{{$article->category->name}}</a>
+                <span class="text-grey"> by {{$article->user->name}}</span>
+            </div>
+            <div class="text-right col-md-4 no-padding">
+                @foreach($article->keywords as $keyword)
+                    <a href="{{route('articles-by-keyword', [$keyword->name])}}">
+                        <span class="label label-info">{{$keyword->name}}</span>
+                    </a>
+                @endforeach
+            </div>
+        </div>
+    </div>
+</div>
