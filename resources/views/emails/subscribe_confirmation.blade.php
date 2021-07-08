3522d4d0023f304e0760a459828b59b2a3a57d2e
Al Imran Ahmed <al.imran.cse@gmail.com>
2021-07-08 14:33:50 +0600

    #81: Email's wording changed

--- resources/views/emails/subscribe_confirmation.blade.php
+++ resources/views/emails/subscribe_confirmation.blade.php
@@ -1,7 +1,7 @@
 @extends('emails.master')
 @section('content')
     <h2>Dear Reader</h2>
-    <p>You have expressed you interested to stay connected with <strong>{{$globalConfigs->site_name}}</strong>
+    <p>You have expressed your interested to stay connected with <strong>{{$globalConfigs->site_name}}</strong>
         Click the link bellow to confirm</p><br>
     <a href="{{route('subscription.confirm')."?token=".$subscriber->token}}">
         Click to confirm
