3264101c673a54f8c9c81adc219db246fdb7c03f
Al Imran Ahmed <al.imran.cse@gmail.com>
2021-08-22 01:56:34 +0200

    Login and dashboard converted to taal

--- resources/views/components/backend/form/input.blade.php
+++ resources/views/components/backend/form/input.blade.php
@@ -2,3 +2,7 @@
 <input type="{{$type}}" name="{{$name}}"
     {{$attributes->merge(['class' => 'p-1 border border-indigo-300 rounded focus:outline-none focus:border-indigo-500'])}}
 >
+
+@error($name)
+<div class="text-red-600">{{$message}}</div>
+@enderror
