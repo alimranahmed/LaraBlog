30c5c1f04beeae5ed4ee4b811a37a486adaf1723
Al Imran Ahmed <al.imran.cse@gmail.com>
2021-12-05 01:26:48 +0100

    Backend is also using app.css

--- resources/views/components/backend.blade.php
+++ resources/views/components/backend.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{asset('build/css/frontend.css')}}">
+    <link rel="stylesheet" href="{{asset('build/css/app.css')}}">
 
     <link rel="stylesheet" href="{{asset('css/prism.css')}}"/>
 
