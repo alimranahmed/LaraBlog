4af9d8d25a2c144346284a7af89047fa5c38cda6
Al Imran Ahmed <al.imran.cse@gmail.com>
2023-01-24 22:57:54 +0100

    Line hight adjusted

--- resources/views/frontend/articles/show.blade.php
+++ resources/views/frontend/articles/show.blade.php
@@ -3,30 +3,30 @@
         <h1 class="sm:text-xl md:text-2xl mb-1 leading-tight">
             {{$article->heading}}
         </h1>
-        <div class="text-gray-600 text-xs md:text-sm mb-3">
+        <div class="text-slate-600 text-xs md:text-sm mb-3">
             Published {{$article->published_date_formatted}} on
             <a href="{{route('articles-by-category', $article->category->alias)}}"
                class="hover:text-blue-700 focus:outline-none focus:text-blue-700">
                 {{$article->category->name}}
             </a>
-            <span class="whitespace-nowrap">by <span class="text-gray-800">{{$article->user->name}}</span></span>
+            <span class="whitespace-nowrap">by <span class="text-slate-800">{{$article->user->name}}</span></span>
         </div>
-        <div class="text-sm leading-relaxed md:text-lg md:leading-loose text-slate-800 article-content">
+        <div class="text-sm leading-relaxed md:text-lg article-content">
             {!! $article->contentAsHtml !!}
         </div>
         @if(!$article->keywords->isEmpty())
-            <div class="mb-3">
+            <div class="my-3">
                 <x-frontend.article.tags :keywords="$article->keywords"></x-frontend.article.tags>
             </div>
         @endif
     </div>
 
     @if(!$relatedArticles->isEmpty())
-        <div class="mb-3">
-            <h2 class="border-b border-blue-300 text-xl md:text-2xl font-medium">
+        <div class="mb-3 mt-5">
+            <h2 class="border-b border-blue-300 text-xl md:text-2xl">
                 More articles on
                 <a href="{{route('articles-by-category', ['categoryAlias' => $article->category->alias])}}"
-                   class="text-blue-400 hover:text-blue-700 focus:outline-none focus:text-blue-700">
+                   class="text-blue-400 hover:text-blue-600 focus:outline-none focus:text-blue-700">
                     {{$article->category->name}}
                 </a>
             </h2>
