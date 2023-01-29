e332dae51fc065e72a19cb7ce91dcc5897730e7f
Al Imran Ahmed <al.imran.cse@gmail.com>
2023-01-29 02:57:16 +0100

    Image added in card

--- resources/views/components/frontend/twitter-card-meta.blade.php
+++ resources/views/components/frontend/twitter-card-meta.blade.php
@@ -4,4 +4,4 @@
 <meta name="twitter:site" content="{{url('/')}}" />
 <meta name="twitter:title" content="{{$article->heading}}" />
 <meta name="twitter:description" content="{{mb_substr($article->content, 0, 152)}}..." />
-<!-- <meta name="twitter:image" content="{{asset('img/user.png')}}" /> -->
+<meta name="twitter:image" content="{{asset('img/user.png')}}" />
