7e45e90b1624ea9d8811415e78daca27292d57bc
Al Imran Ahmed <al.imran.cse@gmail.com>
2023-04-30 23:46:19 +0200

    #147: Fixed the test fails

--- database/migrations/2014_10_12_000001_create_readers_table.php
+++ database/migrations/2014_10_12_000001_create_readers_table.php
@@ -6,12 +6,7 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateReadersTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
+    public function up(): void
     {
         Schema::create('readers', function (Blueprint $table) {
             $table->increments('id');
@@ -27,12 +22,7 @@ class CreateReadersTable extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
+    public function down(): void
     {
         Schema::dropIfExists('readers');
     }
