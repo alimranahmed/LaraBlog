bd0ebe5e485bce89da68d145d0347527c78da4b2
Al Imran Ahmed <al.imran.cse@gmail.com>
2024-06-23 18:45:40 +0200

    #174: Frontend build issue fixed with vite

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,13 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
+import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
 
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        './resources/js/**/*.vue',
     ],
 
     theme: {
@@ -16,5 +18,5 @@ module.exports = {
         },
     },
 
-    plugins: [require('@tailwindcss/forms')],
+    plugins: [forms],
 };
