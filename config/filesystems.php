1d6e543b8c0827a42cad656d641f8f0e29d824c7
Al Imran Ahmed <al.imran.cse@gmail.com>
2024-07-06 14:44:01 +0200

    Fixed missing google drive configuration

--- config/filesystems.php
+++ config/filesystems.php
@@ -56,6 +56,14 @@ return [
             'throw' => false,
         ],
 
+        'google' => [
+            'driver' => 'google',
+            'clientId' => env('GOOGLE_DRIVE_CLIENT_ID'),
+            'clientSecret' => env('GOOGLE_DRIVE_CLIENT_SECRET'),
+            'refreshToken' => env('GOOGLE_DRIVE_REFRESH_TOKEN'),
+            'folder' => env('GOOGLE_DRIVE_FOLDER'), // without folder is root of drive or team drive
+            //'teamDriveId' => env('GOOGLE_DRIVE_TEAM_DRIVE_ID'),
+        ],
     ],
 
     /*
